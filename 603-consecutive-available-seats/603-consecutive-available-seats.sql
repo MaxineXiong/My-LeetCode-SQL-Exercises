87c77584e0fcd5c4896f3f5d79d501eed3b033c7
MaxineXiong <maxinexiong2@gmail.com>
2023-08-15 03:02:45 +1000

    63 solution updated

--- 603-consecutive-available-seats/603-consecutive-available-seats.sql
+++ 603-consecutive-available-seats/603-consecutive-available-seats.sql
@@ -1,1 +1,12 @@
 ----Solution to LeetCode Database question 603: Consecutive Available Seats
+
+SELECT x.seat_id
+FROM
+    (
+      SELECT seat_id, free,
+             LAG(free, 1) OVER (ORDER BY seat_id) AS prev_free,
+             LEAD(free, 1) OVER (ORDER BY seat_id) AS next_free
+      FROM cinema
+    ) AS x
+WHERE (x.free = 1 AND x.prev_free = 1) OR (x.free = 1 AND x.next_free = 1)
+ORDER BY x.seat_id
